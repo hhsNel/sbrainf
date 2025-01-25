1856a28926d65f643f0fc2cfc1cc7ab1e246a304
hhsNel <hihijsonel@gmail.com>
2025-01-25 23:40:17 +0100

    config, args

--- config.h
+++ config.h
@@ -1,13 +1,16 @@
 const struct binding bindings[] = {
 	// name		function		args
-	{ ">",		insertChar,		{.c='>'} },	// move to next cell
-	{ "<",		insertChar,		{.c='>'} },	// move to prev cell
-	{ "+",		insertChar,		{.c='>'} },	// increment current cell
-	{ "-",		insertChar,		{.c='>'} },	// decrement current cell
-	{ ".",		insertChar,		{.c='>'} },	// write current cell
-	{ ",",		insertChar,		{.c='>'} },	// read 1byte to current cell
-	{ "[",		insertChar,		{.c='>'} },	// if current cell is 0, skip to after the corresponding ]
-	{ "]",		insertChar,		{.c='>'} },	// if current cell is not 0, track back to after the corresponding [
-	{ "RESERVE",	changeImmutableOffset,	{}	 },	// mark the next X cells as important	
-	{ "FUNCTION",	makeFunction,		{}	 },	// create a function declaration
+//	{ ">",		insertChar,		{.c='>'} },	// move to next cell
+//	{ "<",		insertChar,		{.c='>'} },	// move to prev cell
+//	{ "+",		insertChar,		{.c='>'} },	// increment current cell
+//	{ "-",		insertChar,		{.c='>'} },	// decrement current cell
+//	{ ".",		insertChar,		{.c='>'} },	// write current cell
+//	{ ",",		insertChar,		{.c='>'} },	// read 1byte to current cell
+//	{ "[",		insertChar,		{.c='>'} },	// if current cell is 0, skip to after the corresponding ]
+//	{ "]",		insertChar,		{.c='>'} },	// if current cell is not 0, track back to after the corresponding [
+//	{ "RESERVE",	changeImmutableOffset,	{}	 },	// mark the next X cells as important	
+//	{ "FUNCTION",	makeFunction,		{}	 },	// create a function declaration
 };
+
+int debug = 1;	// default debug value
+
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 
 struct arg {
 	int i;
@@ -14,12 +15,51 @@ struct binding {
 	struct arg a;
 };
 
-void insertChar(struct arg);
-void changeImmutableOffset(struct arg);
-void makeFunction(struct arg);
+//void insertChar(struct arg);
+//void changeImmutableOffset(struct arg);
+///void makeFunction(struct arg);
 
 #include "config.h"
 
-int main(int argc, char** argv) {
+#define DLOG(msg) if(debug) printf(" Debug: " msg);
+#define DLOGM(msg, ...) if(debug) printf(" Debug: " msg, __VA_ARGS__);
 
+void compile(char *filename) {
+	DLOGM("opening file: %s", filename);
 }
+
+int main(int argc, char** argv) {
+	if(argc == 1) {
+		printf("Usage:\n\
+%s -c file | -r file...", argv[0]);
+		exit(0);
+	}
+	for(int arg = 1; arg < argc; ++arg) {
+		if(argv[arg][0] == '-') {
+			switch(argv[arg][1]) {
+				case 'd':
+				debug = 1;
+				break;
+			case 'D':
+				debug = 0;
+				break;
+			case 'c':
+				if(arg + 1 >= argc) {
+					printf("Expected filename after -c");
+					exit(1);
+				}
+				compile(argv[++arg]);
+				break;
+			// case 'r':
+				//if(arg + 1 >= argc) {
+				//	printf("Expected filename after -r");
+				//	exit(1);
+				//}
+				//run(argv[++arg]);
+				//break;
+			}
+		}
+	}
+}
+
+
